1b1250e964cc1c434ae6233657d14bdcdfc74731
mnaviaz <michelle.navia3@gmail.com>
2018-01-12 12:23:59 -0300

    orden

--- footer.php
+++ footer.php
@@ -1,3 +1,23 @@
+		<footer id="" class="fixed-bottom">
+	        <div class="container">
+	            <div class="row">
+	                <div class="col-sm-3">
+	                    <img class="img-footer" src="<?php echo get_template_directory_uri(); ?>/assets/img/logo_miguelclaro.png" alt="">
+	                </div>
+	                <div class="col-sm-3">
+	                    <h5>Clínica Miguel Claro</h5>
+	                    <ul>
+	                        <li>Miguel Claro 988 - Providencia, Santiago</li>
+	                        <li>Fono: (56 2) 27140600</li>
+	                    </ul>
+	                </div>
+	            </div>
+			</div>
+			<?php wp_footer(); ?>
+		</footer>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+		<script type="text/javascript" src="js/jquery.ba-cond.min.js"></script>
+		<script type="text/javascript" src="js/jquery.slitslider.js"></script>
 		<script type="text/javascript">	
 			$(function() {
 			
@@ -65,3 +85,5 @@
 			
 			});
 		</script>
+	</body>
+</hmtl>
--- front-page.php
+++ front-page.php
@@ -1,4 +1,5 @@
 <?php get_header(); ?>
+	<!-- botoneras redes sociales -->
 	<div class="social">
 		<ul>
 			<li><a href="http://www.facebook.com/falconmasters" target="_blank" class="icon-facebook"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
@@ -6,59 +7,70 @@
 			<li><a href="mailto:contacto@falconmasters.com" class="icon-mail"><i class="fa fa-envelope" aria-hidden="true"></i></a></li>
 		</ul>
 	</div>
-        <div class="container demo-2">
-            <div id="slider" class="sl-slider-wrapper">
-				<div class="sl-slider">
-					<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
-						<div class="sl-slide-inner">
-							<div class="bg-img bg-img-1"></div>
-							<h2>A bene placito.</h2>
-							<blockquote><p>You have just dined, and however scrupulously the slaughterhouse is concealed in the graceful distance of miles, there is complicity.</p><cite>Ralph Waldo Emerson</cite></blockquote>
-						</div>
-					</div>
-					
-					<div class="sl-slide" data-orientation="vertical" data-slice1-rotation="10" data-slice2-rotation="-15" data-slice1-scale="1.5" data-slice2-scale="1.5">
-						<div class="sl-slide-inner">
-							<div class="bg-img bg-img-2"></div>
-							<h2>Regula aurea.</h2>
-							<blockquote><p>Until he extends the circle of his compassion to all living things, man will not himself find peace.</p><cite>Albert Schweitzer</cite></blockquote>
-						</div>
-					</div>
-					
-					<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="3" data-slice2-rotation="3" data-slice1-scale="2" data-slice2-scale="1">
-						<div class="sl-slide-inner">
-							<div class="bg-img bg-img-3"></div>
-							<h2>Dum spiro, spero.</h2>
-							<blockquote><p>Thousands of people who say they 'love' animals sit down once or twice a day to enjoy the flesh of creatures who have been utterly deprived of everything that could make their lives worth living and who endured the awful suffering and the terror of the abattoirs.</p><cite>Dame Jane Morris Goodall</cite></blockquote>
-						</div>
-					</div>
-					
-					<div class="sl-slide" data-orientation="vertical" data-slice1-rotation="-5" data-slice2-rotation="25" data-slice1-scale="2" data-slice2-scale="1">
-						<div class="sl-slide-inner">
-							<div class="bg-img bg-img-4"></div>
-							<h2>Donna nobis pacem.</h2>
-							<blockquote><p>The human body has no more need for cows' milk than it does for dogs' milk, horses' milk, or giraffes' milk.</p><cite>Michael Klaper M.D.</cite></blockquote>
-						</div>
-					</div>
-					
-					<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1">
-						<div class="sl-slide-inner">
-							<div class="bg-img bg-img-5"></div>
-							<h2>Acta Non Verba.</h2>
-							<blockquote><p>I think if you want to eat more meat you should kill it yourself and eat it raw so that you are not blinded by the hypocrisy of having it processed for you.</p><cite>Margi Clarke</cite></blockquote>
-						</div>
-					</div>
-				</div><!-- /sl-slider -->
 
-				<nav id="nav-dots" class="nav-dots">
-					<span class="nav-dot-current"></span>
-					<span></span>
-					<span></span>
-					<span></span>
-					<span></span>
-				</nav>
+	<!-- slider pesado -->
+    <div class="container demo-2">
+        <div id="slider" class="sl-slider-wrapper">
+			<div class="sl-slider">
+				<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-25" data-slice2-rotation="-25" data-slice1-scale="2" data-slice2-scale="2">
+					<div class="sl-slide-inner">
+						<div class="bg-img bg-img-1"></div>
+						<h2>A bene placito.</h2>
+						<blockquote><p>You have just dined, and however scrupulously the slaughterhouse is concealed in the graceful distance of miles, there is complicity.</p><cite>Ralph Waldo Emerson</cite></blockquote>
+					</div>
+				</div>
+				
+				<div class="sl-slide" data-orientation="vertical" data-slice1-rotation="10" data-slice2-rotation="-15" data-slice1-scale="1.5" data-slice2-scale="1.5">
+					<div class="sl-slide-inner">
+						<div class="bg-img bg-img-2"></div>
+						<h2>Regula aurea.</h2>
+						<blockquote><p>Until he extends the circle of his compassion to all living things, man will not himself find peace.</p><cite>Albert Schweitzer</cite></blockquote>
+					</div>
+				</div>
+				
+				<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="3" data-slice2-rotation="3" data-slice1-scale="2" data-slice2-scale="1">
+					<div class="sl-slide-inner">
+						<div class="bg-img bg-img-3"></div>
+						<h2>Dum spiro, spero.</h2>
+						<blockquote><p>Thousands of people who say they 'love' animals sit down once or twice a day to enjoy the flesh of creatures who have been utterly deprived of everything that could make their lives worth living and who endured the awful suffering and the terror of the abattoirs.</p><cite>Dame Jane Morris Goodall</cite></blockquote>
+					</div>
+				</div>
+				
+				<div class="sl-slide" data-orientation="vertical" data-slice1-rotation="-5" data-slice2-rotation="25" data-slice1-scale="2" data-slice2-scale="1">
+					<div class="sl-slide-inner">
+						<div class="bg-img bg-img-4"></div>
+						<h2>Donna nobis pacem.</h2>
+						<blockquote><p>The human body has no more need for cows' milk than it does for dogs' milk, horses' milk, or giraffes' milk.</p><cite>Michael Klaper M.D.</cite></blockquote>
+					</div>
+				</div>
+				
+				<div class="sl-slide" data-orientation="horizontal" data-slice1-rotation="-5" data-slice2-rotation="10" data-slice1-scale="2" data-slice2-scale="1">
+					<div class="sl-slide-inner">
+						<div class="bg-img bg-img-5"></div>
+						<h2>Acta Non Verba.</h2>
+						<blockquote><p>I think if you want to eat more meat you should kill it yourself and eat it raw so that you are not blinded by the hypocrisy of having it processed for you.</p><cite>Margi Clarke</cite></blockquote>
+					</div>
+				</div>
+			</div><!-- /sl-slider -->
 
-			</div><!-- /slider-wrapper -->
+			<nav id="nav-dots" class="nav-dots">
+				<span class="nav-dot-current"></span>
+				<span></span>
+				<span></span>
+				<span></span>
+				<span></span>
+			</nav>
 
-        </div>
+		</div><!-- /slider-wrapper -->
+    </div>
+
+
+    <!-- contenedor equipos -->
+    <div class="container-fluid contenedor-equipos">
+    	<div class="col-md-6 texto-equipos">
+    		<p>Conozca Nuestros</p>
+    		<h1>Equipos y Accesorios</h1>
+    		 <p>de sedación consciente</p>
+    	</div>
+    </div>
 <?php get_footer(); ?>
