fe9fde6050880d0f2c7e15bb2847f9ea7a6b6243
CarlosMellaN <carlosmellaneira@gmail.com>
2018-01-12 12:25:21 -0300

    orden functions
    ordenando slider

--- functions.php
+++ functions.php
@@ -2,17 +2,24 @@
 
 function sedadent_styles(){
 	wp_enqueue_style('normalize', get_stylesheet_directory_uri() . '/css/normalize.css');
-  wp_enqueue_style('custom-slider', get_stylesheet_directory_uri() . '/css/custom-slider.css');
-  wp_enqueue_style('styleNoJS-slider', get_stylesheet_directory_uri() . '/css/styleNoJS-slider.css');
-  wp_enqueue_style('style-slider', get_stylesheet_directory_uri() . '/css/style-slider.css');
   wp_enqueue_style('bootstrap', "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css");
 	//wp_enqueue_style('font-awesome', get_stylesheet_directory_uri() . '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css');
   wp_enqueue_style('font-awesome', get_stylesheet_directory_uri() . '/css/font-awesome.css');
   wp_enqueue_script('jquery');
   wp_enqueue_script('bootstrapjs', "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js", array('jquery'), true);
-   wp_enqueue_script('jquery.ba-cond.min',  get_stylesheet_directory_uri() . '/js/jquery.ba-cond.min.js', array('jquery'), true);
- wp_enqueue_script('jquery.slitslider',  get_stylesheet_directory_uri() . '/js/jquery.slitslider.js', array('jquery'), true);
-wp_enqueue_script('modernizr.custom.79639',  get_stylesheet_directory_uri() . '/js/modernizr.custom.79639.js', array('jquery'), true);
+
+  //CSS Slider
+  wp_enqueue_style('custom-slider', get_stylesheet_directory_uri() . '/css/custom-slider.css');
+  wp_enqueue_style('styleNoJS-slider', get_stylesheet_directory_uri() . '/css/styleNoJS-slider.css');
+  wp_enqueue_style('style-slider', get_stylesheet_directory_uri() . '/css/style-slider.css');
+
+  //Slider jquery
+  wp_enqueue_script('jquery.ba-cond.min',  get_stylesheet_directory_uri() . '/js/jquery.ba-cond.min.js', array('jquery'), true);
+  wp_enqueue_script('jquery.slitslider',  get_stylesheet_directory_uri() . '/js/jquery.slitslider.js', array('jquery'), true);
+  wp_enqueue_script('modernizr.custom.79639',  get_stylesheet_directory_uri() . '/js/modernizr.custom.79639.js', array('jquery'), true);
+
+  //Fin Slider
+
   wp_enqueue_style('slider', get_stylesheet_directory_uri() . '/css/slider.css');
   wp_enqueue_script('jquery');
   wp_enqueue_style('footer', get_stylesheet_directory_uri() . '/css/footer.css');
