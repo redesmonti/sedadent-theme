f65fc2435eafac8e6a977be99377171cefea0933
mnaviaz <michelle.navia3@gmail.com>
2018-01-12 12:03:04 -0300

    publicar

---
+++ functions.php
@@ -0,0 +1,67 @@
+<?php
+
+function sedadent_styles(){
+	wp_enqueue_style('normalize', get_stylesheet_directory_uri() . '/css/normalize.css');
+  wp_enqueue_style('custom-slider', get_stylesheet_directory_uri() . '/css/custom-slider.css');
+  wp_enqueue_style('styleNoJS-slider', get_stylesheet_directory_uri() . '/css/styleNoJS-slider.css');
+  wp_enqueue_style('style-slider', get_stylesheet_directory_uri() . '/css/style-slider.css');
+  wp_enqueue_style('bootstrap', "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css");
+	//wp_enqueue_style('font-awesome', get_stylesheet_directory_uri() . '//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css');
+  wp_enqueue_style('font-awesome', get_stylesheet_directory_uri() . '/css/font-awesome.css');
+  wp_enqueue_script('jquery');
+  wp_enqueue_script('bootstrapjs', "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js", array('jquery'), true);
+   wp_enqueue_script('jquery.ba-cond.min',  get_stylesheet_directory_uri() . '/js/jquery.ba-cond.min.js', array('jquery'), true);
+ wp_enqueue_script('jquery.slitslider',  get_stylesheet_directory_uri() . '/js/jquery.slitslider.js', array('jquery'), true);
+wp_enqueue_script('modernizr.custom.79639',  get_stylesheet_directory_uri() . '/js/modernizr.custom.79639.js', array('jquery'), true);
+  wp_enqueue_style('slider', get_stylesheet_directory_uri() . '/css/slider.css');
+  wp_enqueue_script('jquery');
+  wp_enqueue_style('footer', get_stylesheet_directory_uri() . '/css/footer.css');
+  wp_enqueue_style('style', get_stylesheet_uri()); //usa el style.css, debe ser la ultima hoja de estilos
+}
+add_action('wp_enqueue_scripts', 'sedadent_styles'); //Hook para llamar al la funcion en wordpress
+
+
+//Insertar javascripts
+add_action("wp_enqueue_scripts", "incrustar_js");
+function incrustar_js(){
+ if ( !is_admin() ) { // para que solo haga la carga si no es el área de admin
+    // registra la ubicación, dependencias y versión de su script.
+    wp_register_script('slider',
+        get_template_directory_uri() . '/js/slider.js',
+        array('jquery'),
+        '1.0' );
+
+    wp_register_script('modernizr.custom.79639',
+        get_template_directory_uri() . '/js/modernizr.custom.79639.js',
+        array('jquery'),
+        '1.0' );
+
+    
+    wp_register_script('jquery.ba-cond.min',
+        get_template_directory_uri() . '/js/jquery.ba-cond.min.js',
+        array('jquery'),
+        '1.0' );
+    // pone en cola es script
+    wp_enqueue_script('slider');
+    wp_enqueue_script('modernizr.custom.79639');
+    wp_enqueue_script('jquery.ba-cond.min');
+ }
+}
+
+/*Menus*/
+
+require_once('wp-bootstrap-navwalker.php');
+//Permite agregar los menus
+function mis_menus() {
+  register_nav_menus(
+    array(
+      'navigation' => __( 'Menú de navegación de sedadent'),
+    )
+  );
+}
+add_action( 'after_setup_theme', 'mis_menus' );
+
+/*Añadir imagen destacada*/
+add_theme_support( 'post-thumbnails' );
+
+?>
---
+++ header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="<?php bloginfo('charset'); ?>">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title><?php bloginfo('name'); ?></title>
+    <link rel="stylesheet" href="<?php bloginfo('stylesheet_url') ?>">
+    <?php wp_head(); ?>
+</head>
+<body><!--se cierra en footer.php-->
+	<header class="site-header">
+	    <nav class="navegacion navbar-fixed-top" id="navbarjs" role="navigation">
+			<div class="zona-contactos">
+				<button class="tel"><i class="fa fa-phone" aria-hidden="true"></i> <strong>Tel: </strong>  +56 2 2604 74 59</button>
+				<button class="tel"><i class="fa fa-envelope" aria-hidden="true"></i> <strong>Mail:</strong>  contacto@sedadent.cl</button>
+			</div>
+	        <div class="container">
+	          <div class="navbar-header"> 
+	            <div class="navbar-brand">
+	              <a href="">
+	              	<h3>LOGO</h3>
+	              </a>  
+	            </div> 
+	            <button type="button" class="navbar-toggle smooth-scroll" data-toggle="collapse" data-target=".navbar-ex1-collapse"> 
+	              <span class="sr-only">Toggle navigation</span> 
+	              <span class="icon-bar"></span> 
+	              <span class="icon-bar"></span> 
+	              <span class="icon-bar"></span> 
+	            </button>  
+	          </div>
+	          <div class="smooth-scroll collapse navbar-collapse navbar-right navbar-ex1-collapse">
+	            <?php wp_nav_menu( array( 
+	              'theme_location' => 'navigation',
+	              'depth' => 2,
+	              'container' => false,
+	              'container_id' => 'navbar',
+	              'container_class' => 'collapse navbar-collapse', 
+	              'menu_class' => 'nav navbar-nav navbar-left',
+	              'walker' => new WP_Bootstrap_Navwalker() ) ); ?>    
+	          </div>
+	        </div>        
+	    </nav><!-- fin navegacion-->
+	</header>
